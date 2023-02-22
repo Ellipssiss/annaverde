afb2b2133c6b18e13ad1258310c7d36bdf3f5c15
Nick Mackogonov <nickmackogonov@gmail.com>
2023-02-22 15:42:54 +0300

    CSS_versions_time

--- header.php
+++ header.php
@@ -65,7 +65,8 @@ $headerMainFotoAnotation = getMainPageFotoAnotation();
   <meta charset="UTF-8" />
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/owl.carousel.css" />
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/owl.theme.default.css" />
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css" />
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css?v=<?php echo time(); ?>" />
+  <!-- <link rel="stylesheet" href="styles.css?v=<?php echo time(); ?>"/> -->
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title><? echo $title; ?></title>
